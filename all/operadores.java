5734d7ec9e85f8b9da7b092708bf972e538bb569
77-aguarosada <luisrosada2018@gmail.com>
2021-02-21 17:46:30 -0300

    update

--- all/operadores.java
+++ all/operadores.java
@@ -8,7 +8,12 @@ public class operadores {
     public static void main(String[] args) {
         //utilizando  a class Math 
         //e usando a raiz quadrada.
-        double raiz = Math.sqrt(9);
-        System.out.println(raiz); 
+       // double raiz = Math.sqrt(9);
+       int raiz =(int)Math.sqrt(9);
+
+        //o valor que  vai ser imprimido e double 
+         System.out.println(raiz); 
+        //para ter um int devemos fazer assim
+     
     }
 }
