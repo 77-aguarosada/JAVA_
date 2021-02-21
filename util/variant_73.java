611178b3a54b4c90e377efbbe11aba500bb0525f
77-aguarosada <luisrosada2018@gmail.com>
2021-02-21 16:11:51 -0300

    add file

---
+++ util/variant_73.java
@@ -0,0 +1,56 @@
+
+
+import static java.lang.Math.pow;
+import java.util.Scanner;
+import static java.lang.Math.*;
+
+public class variant_73 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int x, y , F , d,a,b;
+        final double e = 2.71;
+            System.out.print("Vvedite Tchisla   Y ");
+        x = entrada.nextInt();
+        System.out.print("Vvedite Tchisla   a ");
+        a = entrada.nextInt();
+        System.out.print("Vvedite Tchisla   b ");
+        b = entrada.nextInt();
+         System.out.print("Vvedite Tchisla F ");
+        d = entrada.nextInt();
+      
+        if(x<=0){
+            y = (int) (a-pow(b, x+1) )  ;
+             System.out.println(" the value Y :"+y);
+          }
+          if(x>0 && x<=3){
+              
+               y = (int) (1+ pow(3, x));
+               System.out.println(" the value Y :"+ y); 
+          }
+          
+          if(x>3){
+              
+              y = (int) (cos(2*x+1)/sin(2*x+1));
+              System.out.println(" the value Y :"+ y);
+              
+          }
+          if(d<=0){
+              F = (int) pow(e,(sin(2*x+1)/cos(2*x+1)))  ;
+               System.out.println(" the value F :"+F);
+            }
+            if(d>0 && d<=3){
+                
+                 F = (int) (pow(x,2)-(sin(4*d)/cos(4*d)));
+                  System.out.println(" the value F :"+F);
+                 
+            }
+            
+            if(d>3){
+                
+                F = (int) pow(x,2/5);
+                 System.out.println(" the value F :"+F);
+            }
+            
+            
+    }
+}
