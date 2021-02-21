e2c4abdadb820eabd2d278eb721be0f25f25de48
77-aguarosada <luisrosada2018@gmail.com>
2021-02-21 17:38:38 -0300

    class Math

---
+++ all/operadores.java
@@ -0,0 +1,14 @@
+
+
+/**
+ * operadores
+ */
+public class operadores {
+
+    public static void main(String[] args) {
+        //utilizando  a class Math 
+        //e usando a raiz quadrada.
+        double raiz = Math.sqrt(9);
+        System.out.println(raiz); 
+    }
+}
