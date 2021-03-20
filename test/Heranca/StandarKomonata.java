cea1b40b0a143421e82cd281f405904569026995
77-aguarosada <luisrosada2018@gmail.com>
2021-03-20 09:16:37 -0300

    other rooms

---
+++ test/Heranca/StandarKomonata.java
@@ -0,0 +1,8 @@
+package Heranca;
+
+public class StandarKomonata extends komonata{
+    public StandarKomonata (String id,int number,float manyPeople,int price){
+        super(id, number,manyPeople,price);
+
+   }
+}
