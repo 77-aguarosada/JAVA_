4a0d67fb52abce4a520cbc5a6827ae1dd3bde207
77-aguarosada <luisrosada2018@gmail.com>
2021-05-15 09:30:02 +0300

    lab_10

---
+++ Java Практические работы 9-14/lab_10.java
@@ -0,0 +1,47 @@
+import java.util.regex.*;
+ 
+class lab_10{
+ 
+    // Function to validate URL
+    // using regular expression
+    public static boolean
+    isValidURL(String url)
+    {
+        // Regex to check valid URL
+        String regex = "((http|https)://)(www.)?"
+              + "[a-zA-Z0-9@:%._\\+~#?&//=]"
+              + "{2,256}\\.[a-z]"
+              + "{2,6}\\b([-a-zA-Z0-9@:%"
+              + "._\\+~#?&//=]*)";
+ 
+        // Compile the ReGex
+        Pattern p = Pattern.compile(regex);
+ 
+        // If the string is empty
+        // return false
+        if (url == null) {
+            return false;
+        }
+ 
+        // Find match between given string
+        // and regular expression
+        // using Pattern.matcher()
+        Matcher m = p.matcher(url);
+ 
+        // Return if the string
+        // matched the ReGex
+        return m.matches();
+    }
+ 
+    // Driver code
+    public static void main(String args[])
+    {
+        String url
+            = "http://www.zcontest.ru";
+        if (isValidURL(url) == true) {
+            System.out.println("Yes");
+        }
+        else
+            System.out.println("NO");
+    }
+}
